5c0cb625c4c18dbd8e29b28d36d13255cf66b04b
Rgmg16 <gumborobert7@gmail.com>
2024-03-27 20:53:11 +0300

    Conversions

---
+++ conversions.js
@@ -0,0 +1,12 @@
+// Boolean type
+let value = true;
+console.log(typeof(value));
+// Convert to String Value
+let val = String(value);
+console.log(typeof(val));
+// String type
+let val2 = "1";
+console.log(typeof(val2));
+// Convert to Number Value
+let val3 = Number(val2);
+console.log(typeof(val3));
