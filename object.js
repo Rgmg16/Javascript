684e7e96dcba2164114a1a949eacf64e6f859585
Rgmg16 <gumborobert7@gmail.com>
2024-03-28 13:38:20 +0300

    Objects in JS

---
+++ object.js
@@ -0,0 +1,15 @@
+const citizen={
+    country: "Kenya",
+    Age: 21,
+    Gender: "Male",
+    Height: 160,
+    Nmae: "Jeff Owino"
+}
+console.log(citizen);
+
+//Getting object data using dot
+console.log(citizen.Age);
+console.log(citizen.Height);
+
+//Getting object data using quotation marks and box brackets
+console.log(citizen["Gender"]);
