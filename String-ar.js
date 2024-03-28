e22440cb2aea703ab67eace4ece53d4f820a1e18
Rgmg16 <gumborobert7@gmail.com>
2024-03-28 12:17:14 +0300

    String-array and vice versa

---
+++ String-ar.js
@@ -0,0 +1,16 @@
+//converting an array to a string
+let fruits= ["Bananas","Apples","Grapes","Avocadoes"];
+let vegetables= ["Kales","Cucumbers","Spinach","Beans"];
+
+let balance= fruits.concat(vegetables);
+
+string1= balance.toString();
+console.log(string1);
+
+//converting strings into in-line arrays
+arr= string1.split();
+console.log(arr);
+
+//converting strings into list arrays
+arr2= string1.split(',');
+console.log(arr2);
