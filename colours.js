a1d898cb08494eb2faee2f9025c455f1cea85e06
Rgmg16 <gumborobert7@gmail.com>
2024-03-28 12:51:37 +0300

    Colours class excersise

---
+++ colours.js
@@ -0,0 +1,19 @@
+let colours=["red","green","pink","yellow","purple","blue"];
+console.log(colours[1]);
+
+colours.push("yellow");
+console.log(colours);
+colours.shift();
+console.log(colours);
+console.log(colours.reverse());
+console.log(colours.sort());
+console.log(colours.length);
+
+for(let c=0;c<colours.length;c++){
+    console.log(colours[c]);
+}
+colours.pop();
+console.log(colours);
+
+string1= colours.toString();
+console.log(string1);
