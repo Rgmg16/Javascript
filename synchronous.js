fd9def2957c9af7c65896bb953bb2df9e878570e
Rgmg16 <gumborobert7@gmail.com>
2024-04-05 10:24:27 +0300

    Synchronous programming

---
+++ synchronous.js
@@ -0,0 +1,9 @@
+function greet(name){
+    return "Hello " +name;
+}
+
+console.log("Text before function call");
+console.log(greet("Val"));
+console.log(greet("Frank"));
+console.log(greet("Jeff"));
+console.log("Text after function call");
